a6609aaeb913a974ecf8f4b594c4697e373cfee7
Nishant Jadhav <159153967+nishant-1902@users.noreply.github.com>
2025-09-12 18:12:36 +0530

    Add files via upload

---
+++ Day 4/01_tuple_methods.py
@@ -0,0 +1,28 @@
+# len() method
+tuple1 = [22,11,44,66,78,44,21]
+# print(len(tuple1))
+#
+# # max() method
+# print(max(tuple1))
+#
+# # min() method
+# print(min(tuple1))
+#
+# #count() method
+# cnt = tuple1.count(21)
+# print(cnt)
+#
+# # index() method
+# num = tuple1.index(11)
+# print(num)
+
+# reversed() method
+# t = tuple(reversed(tuple1))
+# print(t)
+
+# sum() method
+sum1 = sum(tuple1)
+print(sum1)
+
+#
+
