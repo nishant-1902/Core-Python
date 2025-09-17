f96a926fbea8827f9b0b5ada66701961274c20e5
Nishant Jadhav <159153967+nishant-1902@users.noreply.github.com>
2025-09-17 18:21:41 +0530

    Add files via upload

---
+++ Day 6/01_functions.py
@@ -0,0 +1,23 @@
+def add() :
+    a = 10
+    b = 20
+    print(a+b)
+add()
+
+def sub() :
+    a = 50
+    b = 30
+    print(a-b)
+sub()
+
+def mult() :
+    a = 2
+    b = 4
+    print(a*b)
+mult()
+
+def div() :
+    a = 3
+    b = 6
+    print(b/a)
+div()
---
+++ Day 6/02_parameterise_fuc.py
@@ -0,0 +1,24 @@
+# required argument
+# def add (a,b):
+#     sum = a + b
+#     print(sum)
+# add(10,20)
+
+# keyword argument
+def hello (name,age,city):
+    print("My name is",name,"\nMy age is",age,"\nI am from",city)
+hello("nishant",21,"pune")
+
+# default argument
+# def fortune(a,b=61,c=34):
+#     print("the value of a :",a)
+#     print("the value of b :",b)
+#     print("the value of c :",c)
+# fortune(a=45)
+
+# variable length
+def student(name,**marks):
+    print("Name :",name)
+    print("Marks :",marks)
+st
+
